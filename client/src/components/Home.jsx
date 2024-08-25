fb059ff30561beebbdb918fdb8aa74cb92cb0825
Shivam Maurya <shivammaurya011@gmail.com>
2024-08-25 12:39:04 +0530

    first commit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 const Home = () => {
+  const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  return (
-    <div>
-      <h1>Zoom App</h1>
-    </div>
-  );
+    useEffect(() => {
+        // Fetch data from JSONPlaceholder API
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                setData(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+    return (
+        <div>
+            <h1>Welcome to the Zoom App</h1>
+            <div>
+            <h2>Fetched Data:</h2>
+            <ul>
+                {data.map(item => (
+                    <li key={item.id}>
+                        <h3>{item.title}</h3>
+                        <p>{item.body}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        </div>
+    );
 };
 
 export default Home;
--- client/src/components/ZoomAuth.jsx
+++ client/src/components/ZoomAuth.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 function ZoomAuth() {
     const clientID = 'TCf3V1BOQbuf_v8XjLemUw';
-    const redirectUri = encodeURIComponent('https://zoom-app-snowy.vercel.app/auth');
-    const zoomOAuthUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${clientID}&redirect_uri=${redirectUri}`;
-  return (
-    <div>
-      <h1>Zoom App</h1>
-      <a href={zoomOAuthUrl}>Authorize Zoom</a>
-    </div>
-  )
+    const redirectUri = encodeURIComponent('https://zoom-app-snowy.vercel.app');
+    const zoomOAuthUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${clientID}&redirect_uri=${redirectUri}/auth`;
+
+    return (
+        <div>
+            <h1>Zoom App</h1>
+            <a href={zoomOAuthUrl}>Authorize Zoom</a>
+        </div>
+    );
 }
 
-export default ZoomAuth
+export default ZoomAuth;
